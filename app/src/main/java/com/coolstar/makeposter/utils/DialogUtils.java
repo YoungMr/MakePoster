4251b28aab84448598557d386e9bc0f0240817c3
coolstar1204 <coolstar1204@gmail.com>
2016-03-16 10:05:06 +0800

    first commit

---
+++ app/src/main/java/com/coolstar/makeposter/utils/DialogUtils.java
@@ -0,0 +1,31 @@
+package com.coolstar.makeposter.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by jiguangxing on 2016/3/15.
+ */
+public class DialogUtils {
+
+    public static void showConfirmDialog(Context context, String dlgContent, DialogInterface.OnClickListener okClickListener,DialogInterface.OnClickListener cancelClickListener){
+        new AlertDialog.Builder(context)
+                .setTitle("确认")
+                .setMessage(dlgContent)
+                .setNegativeButton("取消",cancelClickListener)
+                .setPositiveButton("确定",okClickListener)
+                .create()
+                .show();
+    }
+
+    public static void showHintDialog(Context context,String dlgContent,DialogInterface.OnClickListener okClickListener){
+        new AlertDialog.Builder(context)
+                .setTitle("警告")
+                .setMessage(dlgContent)
+                .setPositiveButton("确定",okClickListener)
+                .create()
+                .show();
+    }
+
+}
